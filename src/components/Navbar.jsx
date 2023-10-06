fea9d4687f6d5faeff20c07e94690194bc70f44f
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-06 15:36:07 +0600

    custom font added and search bar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,44 @@
 import navLogo from "../assets/main-logo.png";
 import { Link } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
 const Navbar = () => {
   return (
-    <div  className="flex items-center text-white justify-between pt-9">
+    <div className="flex items-center text-white justify-evenly pt-9">
       <div>
         <img className="w-28 " src={navLogo} alt="" />
       </div>
       <div>
-        <input
-          type="text"
-          placeholder="Search your Destination..."
-          className="input input-ghost w-full max-w-xs"
-        />
+        <label className="flex items-center ">
+          <FiSearch className="-mr-6 "></FiSearch>
+          <input
+            type="text"
+            placeholder={` Search your destination....`}
+            className="input pl-8 input-ghost bg-[#ffffff33] border border-white w-[23rem]  "
+          />
+        </label>
       </div>
       <div className="">
-        <ul className="flex items-center gap-14">
-            <li>
-                <Link>News</Link>
-            </li>
-            <li>
-                <Link>Destination</Link>
-            </li>
-            <li>
-                <Link>Blog</Link>
-            </li>
-            <li>
-                <Link>Contact</Link>
-            </li>
-            <li>
-                <Link> <button className="btn btn-warning capitalize text-black">Login</button> </Link>
-            </li>
+        <ul className="flex font-medium items-center gap-14">
+          <li>
+            <Link>News</Link>
+          </li>
+          <li>
+            <Link>Destination</Link>
+          </li>
+          <li>
+            <Link>Blog</Link>
+          </li>
+          <li>
+            <Link>Contact</Link>
+          </li>
+          <li>
+            <Link>
+              {" "}
+              <button className="btn btn-warning capitalize text-black">
+                Login
+              </button>{" "}
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
